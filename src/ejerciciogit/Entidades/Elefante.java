ff55d01679cbae74855f14e85713f6c939a1703b
Luis Corti <luiscorti2323@gmail.com>
2021-10-15 20:19:37 -0300

    committ

--- src/ejerciciogit/Entidades/Elefante.java
+++ src/ejerciciogit/Entidades/Elefante.java
@@ -11,4 +11,74 @@ package ejerciciogit.Entidades;
  */
 public class Elefante {
     
+
+
+    
+    private String id;
+    private String nombre;
+    private Integer edad;
+    private Integer peso;
+    private String raza;
+
+    public Elefante() {
+    }
+
+    public Elefante(String id, String nombre, Integer edad, Integer peso, String raza) {
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.raza = raza;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public Integer getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Integer peso) {
+        this.peso = peso;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    @Override
+    public String toString() {
+        return "Elefante{" + "id=" + id + ", nombre=" + nombre + ", edad=" + edad + ", peso=" + peso + ", raza=" + raza + '}';
+    }
+
+    
+    
+    
 }
+
+
